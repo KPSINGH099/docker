3df78ef1294e622cc5a4e6fa65866e963bbcd335
Kishan <singh000kishan@gmail.com>
2023-12-31 18:38:25 +0530

    first

---
+++ express/index.js
@@ -0,0 +1,16 @@
+const express= require("express");
+
+const app=express();
+
+const port=process.env.port || 3001;
+
+app.get("/",(req,res)=>{
+console.log('data send');
+res.send("<h1>ka bolti ?</h1>");
+console.log('data send');
+})
+
+
+app.listen(port,()=>{
+    console.log(`running on ${port}`)
+})

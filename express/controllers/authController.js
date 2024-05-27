fdf116e48ece75eed2e2cec0db4459667d625508
Kishan <singh000kishan@gmail.com>
2024-05-27 16:07:51 +0530

    adding login

--- express/controllers/authController.js
+++ express/controllers/authController.js
@@ -8,7 +8,7 @@ exports.signUp= async (req,res,next)=>{
     try{
 
         const hashpassword= await bycrypt.hash(password,12);
-        
+
         const user=await User.create({
             username,
             password:hashpassword,
@@ -20,6 +20,44 @@ exports.signUp= async (req,res,next)=>{
                 user,
             }
         })
+    }catch(e){
+        res.status(400).json({
+            status: "bad req"
+        })
+    }
+   
+};
+
+
+exports.login= async (req,res,next)=>{
+    const {username,password}= req.body;
+  
+    try{
+        
+        const user=await User.findOne({username});
+        //if we dont have user with that username
+       if(!user){
+        res.status(404).json({
+            status: "fail",
+            message: "user not found",
+        })
+       }
+      
+       //if we do have user with that check if password entered by user 
+       //is equal to password stored in db after decrypting it 
+       const isCorrect=await bycrypt.compare(password,user.password)
+
+       if(isCorrect){
+        res.status(200).json({
+            status: "sucess"
+        })
+       }
+      else{
+        res.status(404).json({
+            status: "fail",
+            message: "incorrect password",
+        })
+      }
     }catch(e){
         res.status(400).json({
             status: "bad req"
